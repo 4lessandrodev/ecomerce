250b3cac2ecf7003fc1d98a66c689380100aac18
Alessandro Menezes <alessandroadm@live.com>
2020-03-06 21:48:41 -0300

    Criando rotas para as paginas admin

--- apps_node/routes/admin.js
+++ apps_node/routes/admin.js
@@ -4,21 +4,54 @@ const adminController = require('../controllers/AdminController');
 const categoriaProdutoController = require('../controllers/CategoriaProdutoController');
 
 //------------------------------------------------------------------------------------------------------
+//Metodo para listar os emails de usuarios inscritos 
 router.get('/inscricoes', (req, res, next) => {
   adminController.listarInscricoes(req, res, next);
 });
 //------------------------------------------------------------------------------------------------------
+//Metodo para listar as mensagens de usuarios  
 router.get('/mensagens', (req, res, next) => {
   adminController.listarMensagens(req, res, next);
 });
 //------------------------------------------------------------------------------------------------------
-router.post('/salvar-categoria', (req, res, next) => {
+//Metodo para salvar as categorias de produto
+router.post('/salvar-categoria-produto', (req, res, next) => {
   categoriaProdutoController.salvarCategoria(req, res, next);
 });
 //------------------------------------------------------------------------------------------------------
+//Rota para listar as categorias de produtos
 router.get('/listar-categorias-produto', (req, res, next) => {
   categoriaProdutoController.listarCategorias(req, res, next);
 });
-
-
+//------------------------------------------------------------------------------------------------------
+//Renderizar a pagina principal de admin
+router.get('/', (req, res, next) => {
+  res.render('admin/index', {
+    data: '',
+    navbar: false
+  });
+});
+//------------------------------------------------------------------------------------------------------
+//Renderizar a pagina de admin de categoria de produto
+router.get('/categoria-produtos', (req, res, next) => {
+  res.render('admin/categoria-produtos', {
+    categoriasProd: [],
+    data: '',
+    navbar: true,
+    pagina: 'Categoria produtos',
+    btnLabel: 'categoria produto'
+  });
+});
+//------------------------------------------------------------------------------------------------------
+//Renderizar a pagina de admin de categoria de produto
+router.get('/categoria-produtos', (req, res, next) => {
+  res.render('admin/categoria-produtos', {
+    categoriasProd: [],
+    data: '',
+    navbar: true,
+    pagina: 'Categoria produtos',
+    btnLabel: 'categoria produto'
+  });
+});
+//------------------------------------------------------------------------------------------------------
 module.exports = router;
--- apps_node/routes/index.js
+++ apps_node/routes/index.js
@@ -4,7 +4,7 @@ const indexController = require('../controllers/IndexController');
 
 //------------------------------------------------------------------------------------------------------
 //Rota para renderizar a pagina principal  
-router.get('/index', (req, res, next) => {
+router.get('/', (req, res, next) => {
   res.render('index', { cestas: [], produtos: [] });
 });
 //------------------------------------------------------------------------------------------------------
@@ -13,7 +13,12 @@ router.get('/contact', (req, res, next) => {
   res.render('contact', { body: {} });
 });
 //------------------------------------------------------------------------------------------------------
-//Rota para renderizar a pagina de carrinho
+//Rota para renderizar a pagina de cestas da semana
+router.get('/cestas', (req, res, next) => {
+  res.render('cestas', { cestas: [], produtos: [] });
+});
+//------------------------------------------------------------------------------------------------------
+//Rota para renderizar a pagina de carrinho com os itens adicionados
 router.get('/carrinho', (req, res, next) => {
   res.render('carrinho', { clientLoged: { name: 'Usuario', street: 'Rua sem nome casa sem numero' } });
 });
@@ -26,6 +31,11 @@ router.post('/inscrever', (req, res, next) => {
 //Metodo de enviar mensagem de contato 
 router.post('/mensagem', (req, res, next) => {
   indexController.enviarMensagem(req, res, next);
+});
+//------------------------------------------------------------------------------------------------------
+//Rota para renderizar a pagina de login
+router.get('/login', (req, res, next) => {
+  res.render('login', { body: [] });
 });
 //------------------------------------------------------------------------------------------------------
 module.exports = router;
