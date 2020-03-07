3b73bb60527e5a76245a50d772c8060fc250598d
Alessandro Menezes <alessandroadm@live.com>
2020-03-06 21:11:35 -0300

    Rotas

--- apps_node/controllers/ProdutoCompraController.js
+++ apps_node/controllers/ProdutoCompraController.js
@@ -1,50 +1,43 @@
 const ProdutoCompra = require('./../models/ProdutoCompraModel');
 
-
+//------------------------------------------------------------------
 const salvarProdutoCompra = (req, res, next) => {
   let produtoCompra = new ProdutoCompra(req.body.id_produto, req.body.id_compra);
   produtoCompra.salvarProdutoCompra(produtoCompra).then(produtoCompra => {
     res.send(produtoCompra);
   }).catch(err => {
-
+    res.send(err.message);
   });
 };
-
-
+//------------------------------------------------------------------
 const editarProdutoCompra = (req, res, next) => {
   let produtoCompra = new ProdutoCompra(req.body.id_produto, req.body.id_compra);
-  x.editar().then(xx => {
-
+  produtoCompra.id = req.body.id;
+  produtoCompra.atualizarProdutoCompra(produtoCompra).then(produtoCompra => {
+    res.send(produtoCompra);
   }).catch(err => {
-
+    res.send(err.message);
   });
 };
-
+//------------------------------------------------------------------
 const excluirProdutoCompra = (req, res, next) => {
-  let x = new xxx(req, req, req, req);
-  x.excluir().then(xx => {
-
+  let produtoCompra = new ProdutoCompra();
+  produtoCompra.id = req.body.id;
+  produtoCompra.excluirProdutoCompra(produtoCompra).then(produtoCompra => {
+    res.send(produtoCompra);
   }).catch(err => {
-
+    res.send(err.message);
   });
 };
-
+//------------------------------------------------------------------
 const listarProdutoCompra = (req, res, next) => {
-  let x = new xxx(req, req, req, req);
-  x.listar().then(xx => {
-
+  let produtoCompra = new ProdutoCompra();
+  produtoCompra.listarProdutoCompra(produtoCompra).then(produtoCompra => {
+    res.send(produtoCompra);
   }).catch(err => {
-
+    res.send(err.message);
   });
 };
 
-const desativarProdutoCompra = (req, res, next) => {
-  let x = new xxx(req, req, req, req);
-  x.desativar().then(xx => {
 
-  }).catch(err => {
-
-  });
-};
-
-module.exports = { salvarProdutoCompra, editarProdutoCompra, excluirProdutoCompra, listarProdutoCompra, desativarProdutoCompra };
+module.exports = { salvarProdutoCompra, editarProdutoCompra, excluirProdutoCompra, listarProdutoCompra };
--- apps_node/routes/index.js
+++ apps_node/routes/index.js
@@ -2,11 +2,21 @@ var express = require('express');
 var router = express.Router();
 const indexController = require('../controllers/IndexController');
 
-
+//------------------------------------------------------------------------------------------------------
+//Rota para renderizar a pagina principal  
 router.get('/index', (req, res, next) => {
   res.render('index', { cestas: [], produtos: [] });
 });
-
+//------------------------------------------------------------------------------------------------------
+//Rota para renderizar a pagina de contato
+router.get('/contact', (req, res, next) => {
+  res.render('contact', { body: {} });
+});
+//------------------------------------------------------------------------------------------------------
+//Rota para renderizar a pagina de carrinho
+router.get('/carrinho', (req, res, next) => {
+  res.render('carrinho', { clientLoged: { name: 'Usuario', street: 'Rua sem nome casa sem numero' } });
+});
 //------------------------------------------------------------------------------------------------------
 //Metodo de postagem do email 
 router.post('/inscrever', (req, res, next) => {
