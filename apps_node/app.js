c4e91373b340f9d3fb7bc0beecd014a88ca846ae
Alessandro Menezes <alessandroadm@live.com>
2020-03-20 12:22:47 -0300

    Ajustes e configurações para rodar no servidor

--- apps_node/app.js
+++ apps_node/app.js
@@ -34,6 +34,7 @@ app.use(express.urlencoded({ limit: '10mb', extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 //--------------------------------------------------
 app.use(cookieSession({
   name: 'session',
@@ -49,7 +50,6 @@ app.use(cookieSession({
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/admin', adminRouter);
-
 //--------------------------------------------------
 
 //REALIZAR O UPLOAD DE ARQUIVOS 
@@ -78,12 +78,11 @@ app.post('/images/uploads', (req, res, next) => {
 app.use(function (req, res, next) {
   next(createError(404));
 });
-
 // error handler
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'production' ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
